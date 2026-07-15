143d27a5fb722587c9d89eba1f28dd9baa826f04
unmilley <1337@milley.uno>
2026-07-15 18:52:23 +0500

    chore: Initial

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { test } from '../src/index.ts'
+
+describe('packageName', () => {
+	it('pass', () => {
+		expect(test()).toBe('works!')
+	})
+})
